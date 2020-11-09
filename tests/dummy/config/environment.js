a5134bbd3dbf02b9841aef1782a079c0647de841
betocantu93 <betocantu93@gmail.com>
2020-11-09 10:30:38 -0600

    enable mirage in prod

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -45,6 +45,9 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV['ember-cli-mirage'] = {
+      enabled: true
+    }
   }
 
   return ENV;
