83cedc96ef556806ed55285d027939a18e10dec5
betocantu93 <betocantu93@gmail.com>
2022-06-03 16:47:45 -0500

    v3.22.0...v4.4.0

--- packages/ember-search-ui/ember-cli-build.js
+++ packages/ember-search-ui/ember-cli-build.js
@@ -2,7 +2,7 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     autoImport: {
       webpack: {
@@ -21,5 +21,12 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };
