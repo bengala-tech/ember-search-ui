14aed8a84545b1f283ed9673eb2ad17c3cd22c6e
betocantu93 <betocantu93@gmail.com>
2022-06-03 17:02:25 -0500

    v3.22.0...v4.4.0

--- examples/sandbox/tests/test-helper.js
+++ examples/sandbox/tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'sandbox/app';
 import config from 'sandbox/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
