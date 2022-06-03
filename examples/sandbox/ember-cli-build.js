1b082e492871b2f4a3519c95d4c34e1e5bb7cf5a
betocantu93 <betocantu93@gmail.com>
2022-06-03 17:45:47 -0500

    finish update

--- examples/sandbox/ember-cli-build.js
+++ examples/sandbox/ember-cli-build.js
@@ -3,16 +3,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    autoImport: {
-      webpack: {
-        node: {
-          global: true,
-          fs: 'empty'
-        }
-      }
-    }
-  });
+  let app = new EmberApp(defaults, {});
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
--- packages/ember-search-ui-views/addon/components/autocomplete-input/index.js
+++ packages/ember-search-ui-views/addon/components/autocomplete-input/index.js
@@ -3,28 +3,28 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class MenuComponent extends Menu {
-	@tracked isOpen = true;
-	@action
-	open() {
-		this.isOpen = true;
-	}
+  @tracked isOpen = true;
+  @action
+  open() {
+    this.isOpen = true;
+  }
 
-	@action
-	onInputEnter() {
-		this.args.onInput(this.args.searchText)
-	}
+  @action
+  onInputEnter() {
+    this.args.onInput(this.args.searchText);
+  }
 
-	@action
-	close(focusInput = true) {
-		this.isOpen = false;
-		if(focusInput) {
-			Ember.run.next(() => {
-				document.getElementById(this.buttonGuid).focus();
-			});
-		}
-	}
+  @action
+  close(focusInput = true) {
+    this.isOpen = false;
+    if (focusInput) {
+      Ember.run.next(() => {
+        document.getElementById(this.buttonGuid).focus();
+      });
+    }
+  }
 
-	@action
+  @action
   goToPreviousItem() {
     let previousItem = this.items
       .slice()
@@ -38,17 +38,18 @@ export default class MenuComponent extends Menu {
         }
         return item.isEnabled;
       });
-		this._setActiveItem(previousItem);
-		
-		if(previousItem === undefined) {
-			Ember.run.next(() => {
-				document.getElementById(this.buttonGuid).focus();
-			});
-		}
+    this._setActiveItem(previousItem);
+
+    if (previousItem === undefined) {
+      Ember.run.next(() => {
+        document.getElementById(this.buttonGuid).focus();
+      });
+    }
   }
 
+  @action
+  _onInput() {}
 
-	@action
-	_onInput() {}
-	
+  @action
+  onPointerDown(event) {}
 }
